6c645d4001f0466ce6136ae9f6c97f890c0a99e1
p1mple <p1mple@users.noreply.github.com>
2022-12-16 19:32:04 +0100

    Update
    Removed Offset Hardcode

--- ASCIIFILTER.java
+++ ASCIIFILTER.java
@@ -23,63 +23,69 @@ import javax.imageio.ImageIO;
 
 public class ASCIIFILTER {
     public static void main(String args[]) throws FileNotFoundException {
-        BufferedImage img = null;
-        // getting the image file
         try {
-            img = ImageIO.read(new File(args[0]));
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        // output declarations
-        PrintWriter out = new PrintWriter(new File("./output.txt")); // declaration of the output.txt file
-        String s = ""; // output String
-
-        // getting the image dimensions, for those reading, powers of 2 is recomended,
-        // looks best
-        int image_h = img.getHeight();
-        int image_w = img.getWidth();
-
-        // hardcode brightness offset :/, can change it, just my personal choice
-        int offset = -85;
-
-        // getting the pixel data and turning it into the wierd ascii boxes
-        for (int i = 0; i < image_h; i++) { // "height pointer"
-            for (int j = 0; j < image_w; j++) { // "width pointer"
-                int p = img.getRGB(j, i);
-
-                int image_r = (p >> 16) & 0xff;
-                int image_g = (p >> 8) & 0xff; // getting the single color values
-                int image_b = p & 0xff;
-
-                int image_brightness = (image_b + image_g + image_r) / 3; // calculating brightness
-                if (j == image_w - 1) {
-                    s += "\n";
-                }
-
-                // brightness limits for the right characters, yes looks cursed
-                if (image_brightness <= 64 + offset) {
-                    s += "███";
-                } else if (64 + offset < image_brightness && image_brightness < 129 + offset) {
-                    s += "▓▓▓";
-                } else if (129 + offset < image_brightness && image_brightness < 193 + offset) {
-                    s += "▒▒▒";
-                } else {
-                    s += "░░░";
-                }
+            BufferedImage img = null;
+            // getting the image file
+            try {
+                img = ImageIO.read(new File(args[0]));
+            } catch (IOException e) {
+                System.out.println(e);
             }
+            // output declarations
+            PrintWriter out = new PrintWriter(new File("./output.txt")); // declaration of the output.txt file
+            String s = ""; // output String
 
-            // progress bar
-            int progress = 10 - (int) (((double) (image_h - 1 - i) / (double) image_h) * 10);
-            String progress_string = "";
-            for (int c = 0; c < progress; c++)
-                progress_string += "█";
-            for (int c = 0; c < 10 - progress; c++)
-                progress_string += " ";
-            System.out.println("Progress : [" + progress_string + "]" + progress * 10 + "%");
+            // getting the image dimensions, for those reading, powers of 2 is recomended,
+            // looks best
+            int image_h = img.getHeight();
+            int image_w = img.getWidth();
+
+            // seccond argument of initialization sets the brightness offset [DEFAULT = 85]
+            int offset = -85;
+            if (args.length == 2)
+                offset = -Integer.parseInt(args[1]);
+
+            // getting the pixel data and turning it into the wierd ascii boxes
+            for (int i = 0; i < image_h; i++) { // "height pointer"
+                for (int j = 0; j < image_w; j++) { // "width pointer"
+                    int p = img.getRGB(j, i);
+
+                    int image_r = (p >> 16) & 0xff;
+                    int image_g = (p >> 8) & 0xff; // getting the single color values
+                    int image_b = p & 0xff;
+
+                    int image_brightness = (image_b + image_g + image_r) / 3; // calculating brightness
+                    if (j == image_w - 1) {
+                        s += "\n";
+                    }
+
+                    // brightness limits for the right characters, yes looks cursed
+                    if (image_brightness <= 64 + offset) {
+                        s += "███";
+                    } else if (64 + offset < image_brightness && image_brightness < 129 + offset) {
+                        s += "▓▓▓";
+                    } else if (129 + offset < image_brightness && image_brightness < 193 + offset) {
+                        s += "▒▒▒";
+                    } else {
+                        s += "░░░";
+                    }
+                }
+
+                // progress bar
+                int progress = 10 - (int) (((double) (image_h - 1 - i) / (double) image_h) * 10);
+                String progress_string = "";
+                for (int c = 0; c < progress; c++)
+                    progress_string += "█";
+                for (int c = 0; c < 10 - progress; c++)
+                    progress_string += " ";
+                System.out.println("Progress : [" + progress_string + "]" + progress * 10 + "%");
+            }
+            System.out.println("Done.");
+            out.println(s);
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Usage: java ASCIIFILTER <image_pathname> <offset>");
         }
-        System.out.println("Done.");
-        out.println(s);
-        out.close();
     }
 }
 // 78 79 32 77 65 73 68 69 78 83 63
